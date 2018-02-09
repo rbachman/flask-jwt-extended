0b76921bfc85c36413c84aa3fe30cd10066a8c94
Ryan Bachman <Ryan.Bachman@nuix.com>
2018-02-09 00:56:15 -0500

    last change

--- flask_jwt_extended/view_decorators.py
+++ flask_jwt_extended/view_decorators.py
@@ -130,7 +130,7 @@ def jwt_allowed_roles(roles=[]):
             if not verify_token_claims(jwt_data[config.user_claims_key]):
                 raise UserClaimsVerificationError('User claims verification failed')
             if roles:
-                any_in = any(i in roles for i in jwt_data['user_claims']['roles']):
+                any_in = any(i in roles for i in jwt_data['user_claims']['roles'])
                 if not any_in:
                     raise UserClaimsVerificationError('User not a member of roles authorized for this endpoint.')
             _load_user(jwt_data[config.identity_claim_key])
